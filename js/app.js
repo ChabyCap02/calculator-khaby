60e3dd311146bfbbfe135130e17ebc4a449bc3c6
FCPjunior <90715542+ChabyCap02@users.noreply.github.com>
2021-12-08 16:16:40 -0500

    add ultimas funcionalidades en mi calculadora

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-
+//control de signo, si este esta activado se puede agregar
+let signo = false;
+let nNegativo = true
 
 //Pantalla
 let operacion =document.getElementById('operacion');
@@ -6,67 +8,74 @@ let resultado = document.getElementById('resultado')
 
 const agregarNumero = (tecla) => {
     operacion.innerHTML += tecla.innerHTML;
-    calculo.push(tecla.innerHTML)
+    calculo.push(tecla.innerHTML);
+    console.log(calculo);
+    signo = true
 }
 
 //Funcion Suma
 
-let calculo = operacion.innerHTML.split(' ');
-let signo = true;
+let calculo = operacion.innerHTML.split('');
 const sumar = () => {
 
-    
-    if(signo){
+    if(signo == true){
         calculo.push('+');
-        operacion.innerHTML += ' + ';
-        
-    }
-    
-    console.log(calculo)
-}
+        operacion.innerHTML += '+'.repeat(1);
+        signo = false;
+    };
+    console.log(calculo);
+};
 
-const signo = () => {
-    
-}
 
 const restar = () => {
-    
-    operacion.innerHTML += ' - ';
-}
-
+    if(signo == true || nNegativo == true){
+        calculo.push('-');
+        operacion.innerHTML += '-'.repeat(1);
+        signo = false;
+        nNegativo = false
+    };
+    console.log(calculo);
+};
+//Funcion para dividir
 const dividir = () => {
-    
-    operacion.innerHTML += ' / ';
-}
+    if(signo == true){
+        calculo.push('/');
+        operacion.innerHTML += '/'.repeat(1);
+        signo = false;
+    };
+    console.log(calculo);
+};
+//Funcion para multiplicar
 const multiplicar = () => {
-    
-    operacion.innerHTML += ' * ';
-}
+    if(signo == true){
+        calculo.push('+');
+        operacion.innerHTML += '+'.repeat(1);
+        signo = false;
+    };
+    console.log(calculo);
+};
 
 //Funcion IGUal
 const igual = () => {
-    
-    
-
-
     let resultado1 = eval(operacion.innerHTML);
     resultado.innerHTML = resultado1;
-    operacion.style.overflow = 'hidden';
     
-    
-}
+     
+};
 
 const reset  = () => {
     operacion.innerHTML = '';
     resultado.innerHTML = '';
-    calculo = []
+    calculo = [];
+    signo = false;
 
 }
 
 const eliminar = () => {
-
-    let operacion1
-    operacion1.innerHTML.substring(0, operacion.innerHTML.length - 1)
-    operacion.innerHTML = operacion1;
+    calculo.pop(calculo[-1]);
+    console.log(calculo);
+    operacionR = calculo.join('');
+    
+    operacion.innerHTML = operacionR;
 }
 
